eea07628f80d40ccb1d9c3cf066c52232a2437e0
carl <karol-nowakowski@o2.pl>
2020-12-22 22:15:20 +0100

    Apollo client implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {ApolloClient, InMemoryCache, HttpLink, gql, ApolloProvider} from "@apollo/client";
 import './index.css';
 import App from './App';
 
+const URL = "https://salty-cove-78545.herokuapp.com/";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: URL
+  })
+});
+
+const RANDOM_QUOTE_QUERY = gql`
+query getRandomQuote {
+  randomQuote {
+    text
+    author
+  }
+}
+`;
+client.query({ query: RANDOM_QUOTE_QUERY}).then(result => console.log("Query result:", result.data));
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
